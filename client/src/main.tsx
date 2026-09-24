48c3e5d1c901a44a40cdd87e16a953eaceb9d18c
akbarnawasunda <2.56219392e+08+akbarnawasunda@users.noreply.github.com>
2026-09-24 01:02:41 +0000

    feat: redesign public site with cinematic editorial system

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import { createRoot } from "react-dom/client";
 import superjson from "superjson";
 import App from "./App";
 import "./index.css";
+import "./CinematicReference.css";
 
 const queryClient = new QueryClient();
 
